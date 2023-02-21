a89292e0592a3263eea1af1c22313b0070cf8874
gjojo1234 <gensor1234@gmail.com>
2023-02-21 10:19:09 +0100

    update feedback-headers2

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
       const { data } = await axios.get(`${BASE_URL}/feedback`, {
         headers: {
           "Content-Type": "application/json;charset=UTF-8",
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin": `${BASE_URL}/feedback`,
         },
       });
 
